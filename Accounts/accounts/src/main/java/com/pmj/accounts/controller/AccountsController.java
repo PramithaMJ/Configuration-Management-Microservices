3713bf9338d2384b0c8fd38ed1825b65a4079dfc
Pramitha Jayasooriya <lpramithamj@gmail.com>
2024-02-29 14:59:05 +0530

    configuration properties

--- Accounts/accounts/src/main/java/com/pmj/accounts/controller/AccountsController.java
+++ Accounts/accounts/src/main/java/com/pmj/accounts/controller/AccountsController.java
@@ -1,6 +1,7 @@
 package com.pmj.accounts.controller;
 
 import com.pmj.accounts.constants.AccountsConstants;
+import com.pmj.accounts.dto.AccountContactInfoDto;
 import com.pmj.accounts.dto.CustomerDTO;
 import com.pmj.accounts.dto.ErrorResponseDTO;
 import com.pmj.accounts.dto.ResponseDTO;
@@ -13,9 +14,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -41,6 +42,12 @@ public class AccountsController {
 
     @Value("${build.version}")
     private String buildVersion;
+
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private AccountContactInfoDto accountContactInfoDto;
 
     @Operation(
             summary = "Create Account REST API",
@@ -179,7 +186,6 @@ public class AccountsController {
                     content = @Content(
                             schema = @Schema(implementation = ErrorResponseDTO.class)
                     )
-
             )
     }
     )
@@ -187,4 +193,54 @@ public class AccountsController {
     public ResponseEntity<String> getBuildInfo(){
         return ResponseEntity.status(HttpStatus.OK).body(buildVersion);
     }
+
+
+    @Operation(
+            summary = "Get Java version",
+            description = "Get Java version details that is installed into account microservice"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDTO.class)
+                    )
+            )
+    }
+    )
+    @GetMapping("/java-version")
+    public ResponseEntity<String> getJavaVersion(){
+        return ResponseEntity.status(HttpStatus.OK).body(environment.getProperty("JAVA_HOME"));
+    }
+
+
+    @Operation(
+            summary = "Get Contact Info",
+            description = "Contact Info details that can be reached out in case of any issue"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDTO.class)
+                    )
+            )
+    }
+    )
+    @GetMapping("/contact-info")
+    public ResponseEntity<AccountContactInfoDto> getContactInfo(){
+        return ResponseEntity.status(HttpStatus.OK).body(accountContactInfoDto);
+    }
+
+
 }
